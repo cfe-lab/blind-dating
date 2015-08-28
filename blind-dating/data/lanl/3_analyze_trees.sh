ee85e5e709e7c779512be2e5a3b69527c5e91f1f
Joshua Horacsek <joshua_horacsek@sfu.ca>
2015-08-28 16:11:51 -0700

    Last update

--- blind-dating/data/lanl/3_analyze_trees.sh
+++ blind-dating/data/lanl/3_analyze_trees.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-# Do the analysis on all the output trees
-for f in tree/*; do 
-	g=${f#tree/} ;
-	./analyze.tree.R ${f} 0 analysis/${g%.tre}.pdf
-	./analyze.tree.R ${f} 1 analysis/rtt_${g%.tre}.pdf
-done
+# Before this is run, make sure that all the "good" trees have
+# been put into /trees.good/
+r -f analyze.all.tree.R
+
+# Old: Do the analysis on all the output trees
+#for f in tree/*; do 
+#	g=${f#tree/} ;
+#	r -f analyze.tree.R ${f} 0 analysis/${g%.tre}.pdf
+#	r -f analyze.tree.R ${f} 1 analysis/rtt_${g%.tre}.pdf
+#done
