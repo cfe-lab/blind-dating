bec896b47beb059d2bdd308079d9a0c6cddb32cf
brj1 <brj1@sfu.ca>
2016-03-01 15:44:56 -0800

    data/latent_sim/RUN.sh - fixed bug tex/blind-dating.tex - small changes                      - fixed ancre stats                      - more info in Results                      - added mote on negative slopes in the regression                      - added figure for quadratic regression

--- data/latent_sim/RUN.sh
+++ data/latent_sim/RUN.sh
@@ -6,10 +6,10 @@ for u in 1 0.01 0.0001; do
 	sh ../common/1_simulate_sequences.sh $u $l 19041989 19041989
 	sh ../common/2_build_trees.sh HIV 0
 	sh ../common/3_analysis.sh trees 2 2
-	mv latency\.pdf latency\.${l}\.\${u}\.pdf
-	mv plot\.pdf plot\.${l}\.\${u}\.pdf
-	mv hist\.pdf hist\.${l}\.\${u}\.pdf
-	mv stats\.csv stats\.${l}\.\${u}\.csv
-	mv data\.csv data\.${l}\.\${u}\.csv
+	mv latency\.pdf latency\.${l}\.${u}\.pdf
+	mv plot\.pdf plot\.${l}\.${u}\.pdf
+	mv hist\.pdf hist\.${l}\.${u}\.pdf
+	mv stats\.csv stats\.${l}\.${u}\.csv
+	mv data\.csv data\.${l}\.${u}\.csv
 done;
 done;
