7d9849fe5a376e201b8455ebaed54e3021af001f
brj1 <brj1@sfu.ca>
2016-03-02 11:21:43 -0800

    RUN.sh - uses RAxML again

--- data/latent_sim/RUN.sh
+++ data/latent_sim/RUN.sh
@@ -4,9 +4,9 @@
 for l in 1 0.01 0.0001; do
 for u in 1 0.01 0.0001; do
 	sh ../common/1_simulate_sequences.sh $u $l 19041989 19041989
-	sh ../common/2_build_trees.sh HIV 0
+	sh ../common/2_build_trees.sh HIV 1
 	sh ../common/3_analysis.sh trees 2 2
-	mv latency\.pdf latency\.${l}\.${u}\.pdf
+	mv latency\.csv latency\.${l}\.${u}\.csv
 	mv plot\.pdf plot\.${l}\.${u}\.pdf
 	mv hist\.pdf hist\.${l}\.${u}\.pdf
 	mv stats\.csv stats\.${l}\.${u}\.csv
