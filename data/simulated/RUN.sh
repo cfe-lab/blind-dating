ae5fd4ae2d961d586f768c5f0d60d3025a209b3d
Brad Jones <brj1@sfu.ca>
2017-10-16 17:00:15 -0700

    fixed RUN.sh

--- data/simulated/RUN.sh
+++ data/simulated/RUN.sh
@@ -1,5 +1,15 @@
 # runs test for simulated data
-# MUST be run with data/simulated/ as the working directory
-sh ../common/1_simulate_sequences.sh 0 0 91409891 91409891
-sh ../common/2_build_trees.sh HIV 1
-sh ../common/3_analysis.sh trees 1 2 91409891 '' "Simulated w/o latency"
+# SBATCH --cpus=4
+# SBATCH --mem=2G
+
+dataset=${SIM_9_TRUE}
+rep=${SLURM_ARRAY_INDEX_ID}
+src=~/blind-dating/src/
+raxml=raxmlHPC-PTHREADS-AVX
+
+echo "Building"
+R --slave --silent -f ${src}/build.tree.R --args aligned/${pat_id}.fasta trees/${pat_id}.nwk ${raxml} 1 ${rep}
+echo "Rooting"
+R --slave --silent -f ${src}/root.tree.R --args trees/${pat_id}.nwk info/patient_1943.csv trees.rooted/${pat_id}.rtt.nwk 1
+echo "Regressing"
+R --slave --silent -f ${src}/regression.R --args --tree=trees.rooted/${pat_id}.rtt.nwk --info=info/patient_1943.csv --patid=${pat_id}
