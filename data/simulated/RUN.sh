ed47353b2e4769885f957a76f29159781f3097ba
brj1 <brj1@sfu.ca>
2017-10-23 11:52:29 -0700

    fixing simulated scripts

--- data/simulated/RUN.sh
+++ data/simulated/RUN.sh
@@ -10,6 +10,6 @@ raxml=raxmlHPC-PTHREADS-AVX
 echo "Building"
 R --slave --silent -f ${src}/build.tree.R --args aligned/${pat_id}.fasta trees/${pat_id}.nwk ${raxml} 1 ${rep}
 echo "Rooting"
-R --slave --silent -f ${src}/root.tree.R --args trees/${pat_id}.nwk info/patient_1943.csv trees.rooted/${pat_id}.rtt.nwk 1
+R --slave --silent -f ${src}/root.tree.R --args trees/${pat_id}.nwk info/${pat_id}.csv trees.rooted/${pat_id}.rtt.nwk 1
 echo "Regressing"
 R --slave --silent -f ${src}/regression.R --args --tree=trees.rooted/${pat_id}.rtt.nwk --info=info/patient_1943.csv --patid=${pat_id}
