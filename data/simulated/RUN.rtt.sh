0388f454207bd587d63a55c690c0a3bd680c3560
Brad Jones <brj1@sfu.ca>
2017-10-25 17:15:42 -0700

    fixed simulated scripts

--- data/simulated/RUN.rtt.sh
+++ data/simulated/RUN.rtt.sh
@@ -1,15 +1,16 @@
-# runs test for simulated data
-# SBATCH --cpus=4
-# SBATCH --mem=2G
+#!/bin/bash
+#SBATCH --cpus=4
+#SBATCH --mem=2G
 
-dataset=${SIM_9_TRUE}
-rep=${SLURM_ARRAY_INDEX_ID}
+rep=$SLURM_ARRAY_TASK_ID
+pat_id=SIM_${rep}
 src=~/blind-dating/src/
 raxml=raxmlHPC-PTHREADS-AVX
+cpus=$SLURM_CPUS_PER_TASK
 
 echo "Building"
-R --slave --silent -f ${src}/build.tree.R --args aligned/${pat_id}.fasta trees/${pat_id}.nwk ${raxml} 1 ${rep}
+R --slave --silent -f ${src}/build.tree.R --args simulated/${pat_id}_TRUE.fas trees/${pat_id}.nwk ${raxml} ${cpus} ${rep}
 echo "Rooting"
-R --slave --silent -f ${src}/root.tree.R --args trees/${pat_id}.nwk info/${pat_id}.csv trees.rooted/${pat_id}.rtt.nwk 1
+R --slave --silent -f ${src}/root.tree.R --args trees/${pat_id}.nwk info/${pat_id}.csv trees.rooted/${pat_id}.rtt.nwk 1 0
 echo "Regressing"
-R --slave --silent -f ${src}/regression.R --args --tree=trees.rooted/${pat_id}.rtt.nwk --info=info/patient_1943.csv --patid=${pat_id}
+R --slave --silent -f ${src}/regression.R --args --tree=trees.rooted/${pat_id}.rtt.nwk --info=info/${pat_id}.csv --patid=${pat_id}
