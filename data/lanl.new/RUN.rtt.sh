7b5b458c3d66cfe2a3e80c9572cb353cb433bfa6
Brad Jones <brj1@sfu.ca>
2017-10-27 13:58:37 -0700

    fixed lanl RUN scripts

--- data/lanl.new/RUN.rtt.sh
+++ data/lanl.new/RUN.rtt.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+#SBATCH --mem=2G
+#SBATCH --cpus-per-task=4
 
-pat_id=$1
-src=~/git/blind-dating/src
+raxml=raxmlHPC-PTHREADS-AVX
+pat_id=$SLURM_JOB_NAME
+src=~/blind-dating/src
+rep=1989
+cpus=$SLURM_CPUS_PER_TASK
 
+if [ ! -e trees ]; then mkdir trees; fi
+if [ ! -e trees.rooted ]; then mkdir trees.rooted; fi
+if [ ! -e stats ]; then mkdir stats; fi
+
+#echo "Building Tree"
+#R --slave --silent -f ${src}/build.tree.R --args aligned/${pat_id}.fasta trees/${pat_id}.nwk ${raxml} $cpus ${rep}
 echo "Rooting"
-Rscript ${src}/root.tree.R trees/${pat_id}.rtt.nwk info/${pat_id}.csv trees.rooted/${pat_id}.rtt.nwk 1 0
+Rscript ${src}/root.tree.R trees/${pat_id}.nwk info/${pat_id}.csv trees.rooted/${pat_id}.rtt.nwk 1 0
 echo "Regressing"
-Rscript ${src}/regression.R --tree=trees.rooted/${pat_id}.rtt.nwk --info=info/${pat_id}.csv --patid=${pat_id}.rtt --real
+Rscript ${src}/regression.R --tree=trees.rooted/${pat_id}.rtt.nwk --info=info/${pat_id}.csv --patid=${pat_id}
 #echo "Plotting"
 #Rscript ${src}/plot.R --tree=trees.rooted/${pat_id}.ogr.nwk --patid=${pat_id} --real --distmax=0.27 --distby=.04 --yearstart=1993 --yearend=2018 --therapy="2006-08-01"
