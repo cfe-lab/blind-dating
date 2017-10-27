f962c4a5294e3014afa666798c503aff2335e699
brj1 <brj1@sfu.ca>
2017-10-27 11:50:27 -0700

    added lanl

---
+++ data/lanl.new/RUN.rtt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pat_id=$1
+src=~/git/blind-dating/src
+
+echo "Rooting"
+Rscript ${src}/root.tree.R trees/${pat_id}.rtt.nwk info/${pat_id}.csv trees.rooted/${pat_id}.rtt.nwk 1 0
+echo "Regressing"
+Rscript ${src}/regression.R --tree=trees.rooted/${pat_id}.rtt.nwk --info=info/${pat_id}.csv --patid=${pat_id}.rtt --real
+#echo "Plotting"
+#Rscript ${src}/plot.R --tree=trees.rooted/${pat_id}.ogr.nwk --patid=${pat_id} --real --distmax=0.27 --distby=.04 --yearstart=1993 --yearend=2018 --therapy="2006-08-01"
