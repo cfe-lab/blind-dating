e864f4c12fedbda38a33073d31519bc958ef22b8
Brad Jones <brj1@sfu.ca>
2017-11-02 15:16:26 -0700

    rms data

--- data/lanl.new/RUN.rtt-rms.sh
+++ data/lanl.new/RUN.rtt-rms.sh
@@ -4,7 +4,7 @@
 
 raxml=raxmlHPC-PTHREADS-AVX
 pat_id=$SLURM_JOB_NAME
-full_pat_id=${pat_id}-rms
+pat_id_full=${pat_id}-rms
 src=~/blind-dating/src
 rep=1989
 cpus=$SLURM_CPUS_PER_TASK
@@ -16,8 +16,8 @@ if [ ! -e stats ]; then mkdir stats; fi
 #echo "Building Tree"
 #R --slave --silent -f ${src}/build.tree.R --args aligned/${pat_id}.fasta trees/${pat_id}.nwk ${raxml} $cpus ${rep}
 echo "Rooting"
-Rscript ${src}/root.tree.R trees/${pat_id}.nwk info/${pat_id}.csv trees.rooted/${pat_id_full}.rtt.nwk 1 0
+Rscript ${src}/root.tree.R trees/${pat_id}.nwk info/${pat_id}.csv trees.rooted/${pat_id_full}.rtt.nwk 1 0 rms
 echo "Regressing"
-Rscript ${src}/regression.R --tree=trees.rooted/${pat_id_full}.rtt.nwk --info=info/${pat_id}.csv --patid=${pat_id_full}
+Rscript ${src}/regression.R --tree=trees.rooted/${pat_id_full}.rtt.nwk --info=info/${pat_id}.csv --patid="${pat_id_full}"
 #echo "Plotting"
 #Rscript ${src}/plot.R --tree=trees.rooted/${pat_id}.ogr.nwk --patid=${pat_id} --real --distmax=0.27 --distby=.04 --yearstart=1993 --yearend=2018 --therapy="2006-08-01"
