e50caeea8573257184844e8e3c02723d976c7178
Brad Jones <brj1@sfu.ca>
2017-12-04 15:10:46 -0800

    redid simulations

--- data/simulated/BUILD_SEQUENCES.sh
+++ data/simulated/BUILD_SEQUENCES.sh
@@ -3,8 +3,4 @@
 
 rep=$SLURM_ARRAY_TASK_ID
 
-Rscript ../../src/gen_guide_trees.R ${rep} ${rep} ${rep} ref/HXB2.fasta
-#python ../../src/fix_control.py control.unfixed.txt control.txt
-#~/bin/indelible
-#mv SIM_${rep}.fas simulated/
-#mv SIM_${rep}_TRUE.fas simulated/
+Rscript ../../src/gen_guide_trees.R ${rep} ${rep} 1989 ref/HXB2.fasta
--- data/simulated/BUILD_SEQUENCES_2.sh
+++ data/simulated/BUILD_SEQUENCES_2.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 #SBATCH --mem=1G
 
+reps=$1
+
+Rscript ../../src/gen_guide_trees.R head 1 1989 ref/HXB2.fasta
+
 cat control.unfixed.head.txt > control.unfixed.txt
 
-for i in $(seq 1 1000); do head -n 1 control.unfixed.${i}.txt; done >> control.unfixed.txt
-for i in $(seq 1 1000); do head -n 2 control.unfixed.${i}.txt | tail -n 1; done >> control.unfixed.txt
+for i in $(seq 1 ${reps}); do head -n 1 control.unfixed.${i}.txt; done >> control.unfixed.txt
+for i in $(seq 1 ${reps}); do head -n 2 control.unfixed.${i}.txt | tail -n 1; done >> control.unfixed.txt
 echo '[EVOLVE]' >> control.unfixed.txt
-for i in $(seq 1 1000); do head -n 3 control.unfixed.${i}.txt | tail -n 1; done >> control.unfixed.txt
+for i in $(seq 1 ${reps}); do head -n 3 control.unfixed.${i}.txt | tail -n 1; done >> control.unfixed.txt
 
 python ../../src/fix_control.py control.unfixed.txt control.txt
 ~/bin/indelible
-mv *.fas simulated/
+
+Rscript ../../src/censor.simulated.R info/ 50 1989
