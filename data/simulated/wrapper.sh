e9cee3faa481edd83302e31e08b5d45d09f175b1
Brad Jones <brj1@sfu.ca>
2017-12-12 16:03:22 -0800

    updated wrapper

--- data/simulated/wrapper.sh
+++ data/simulated/wrapper.sh
@@ -2,8 +2,13 @@
 #SBATCH --mem=2G
 #SBATCH --cpus-per-task=4
 
-INCR=50
-END=1000
+if [ -z "$SLURM_WRAPPER_INCR" ]; then
+SLURM_WRAPPER_INCR=1
+fi
+
+if [ -z "$SLURM_WRAPPER_END" ]; then
+SLURM_WRAPPER_END=10
+fi
 
 name=$SLURM_JOB_NAME
 inst=$SLURM_ARRAY_TASK_ID
@@ -16,9 +21,9 @@ raxml=raxmlHPC-PTHREADS-AVX
 cpus=$SLURM_CPUS_PER_TASK
 
 source ${name} && \
-new_inst=$(expr "$inst" + "$INCR") && \
+new_inst=$(expr "$inst" + "$SLURM_WRAPPER_INCR") && \
 echo "next: "$new_inst && \
-if [[ $new_inst -le $END ]]; then
-  echo sbatch --get-user-env --mail-type=FAIL --mail-user=bjones@cfenet.ubc.ca --partition=slow --array=$new_inst --job-name=$name wrapper.sh
-  sbatch --get-user-env --mail-type=FAIL --mail-user=bjones@cfenet.ubc.ca --partition=slow --array=$new_inst --job-name=$name wrapper.sh
+if [[ $new_inst -le $SLURM_WRAPPER_END ]]; then
+  echo /opt/scyld/slurm/bin/sbatch --get-user-env --export="SLURM_WRAPPER_INCR=$SLURM_WRAPPER_INCR,SLURM_WRAPPER_END=$SLURM_WRAPPER_END" --mail-type=FAIL --mail-user=bjones@cfenet.ubc.ca --partition=slow --array=$new_inst --job-name=$name wrapper.sh
+  /opt/scyld/slurm/bin/sbatch --get-user-env --export="SLURM_WRAPPER_INCR=$SLURM_WRAPPER_INCR,SLURM_WRAPPER_END=$SLURM_WRAPPER_END" --mail-type=FAIL --mail-user=bjones@cfenet.ubc.ca --partition=slow --array=$new_inst --job-name=$name wrapper.sh
 fi
