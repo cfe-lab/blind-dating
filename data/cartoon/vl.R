b33961051b03ce6447db9da60383b6414082e21d
brj1 <brj1@sfu.ca>
2018-05-02 14:54:25 -0700

    added coloured by sample date for cartoon

--- data/cartoon/vl.R
+++ data/cartoon/vl.R
@@ -20,20 +20,12 @@ panel.grid.minor=element_blank()
 
 THERAPY_COLOUR <- "#a0a0a0"
 THERAPY <- 4.4666666
-data <- read.csv("stats/cartoon.data.csv")
-data <- subset(data, Censored == 1)
 
-pdf.options(family="Helvetica", fonts="Helvetica", width=7, height=4.2, colormodel='rgb')
-pdf("plots/cartoon.hist.pdf")
-ggplot(data, aes(x=Estimated.Date)) +
-	geom_rect(xmin=THERAPY, xmax=Inf, ymin=-Inf, ymax=Inf, fill=THERAPY_COLOUR) +
-	geom_histogram(breaks=seq(-2, 5, by=1), fill='red') +
-	geom_segment(x=0, xend=0, y=2, yend=1.5, arrow=arrow(length = unit(0.5, "cm")))  +
-	theme_bw() +
-	my.theme +
-	scale_x_continuous(name="Years since first collection", breaks=seq(-2, 5, by=1)) +
-	scale_y_continuous(name="Frequency", breaks=c(0, 1, 2))
-dev.off()
+
+THERAPY_COLOUR <- "#111111"
+THERAPY_LTY <- 3
+THERAPY_COLOUR2 <- "#111111"
+THERAPY_LTY2 <- 6
 
 my.theme <- theme(
 legend.justification=c(1, 1),
@@ -54,16 +46,24 @@ legend.key=element_rect(fill="#00000000", colour="#00000000")
 
 data <- read.csv("VL.cartoon.csv")
 
+data$my.colour <- as.numeric(data$Date)
+data$my.colour[data$Censored > 0] <- "censored"
+my.colour.break <- unique(data$my.colour)
+my.colour.filter <- suppressWarnings(!is.na(as.numeric(my.colour.break)))
+my.colour.value <- rep('black', length(my.colour.break))
+my.colour.scale <- (as.numeric(my.colour.break[my.colour.filter]) - 0) / (4.166666666 - 0)
+my.colour.value[my.colour.filter] <- hsv(my.colour.scale * .75, 0.5, 0.5)
+
 pdf.options(family="Helvetica", fonts="Helvetica", width=7, height=4.2, colormodel='rgb')
 pdf("plots/VL.cartoon.pdf")
 ggplot(data, aes(x=Date, y=VL)) +
-	geom_rect(xmin=THERAPY, xmax=Inf, ymin=-Inf, ymax=Inf, fill=THERAPY_COLOUR) +
+	geom_vline(xintercept=as.numeric(THERAPY), linetype=THERAPY_LTY, colour=THERAPY_COLOUR) +
 	geom_line(size=1) +
 	theme_bw() +
 	my.theme +
 	coord_cartesian(ylim=c(10, 120000), xlim=c(0, 8)) +
 	scale_y_log10(name="Viral load", breaks=10^c(1, 3, 5), labels=sapply(c(1, 3, 5), function(x) bquote(''*10^{.(x)}*''))) +
 	scale_x_continuous(name="Years since first collection", breaks=seq(0, 8)) +
-	geom_point(aes(colour=factor(Censored), shape=Type), data=subset(data, Used != ""), size=8) +
-	scale_colour_manual(name="", breaks=c(0, 1), limits=c(0, 1), labels=c("Training", "Censored"), values=c('black', 'red')) + scale_shape_manual(name="", breaks=c("RNA", "DNA"), limits=c("RNA", "DNA"), labels=c("RNA", "DNA"), values=c(16, 5))
+	geom_point(aes(colour=factor(my.colour), shape=Type), data=subset(data, Used != ""), size=8) +
+	scale_colour_manual(name="", breaks=my.colour.break, limits=my.colour.break, values=my.colour.value) + scale_shape_manual(name="", breaks=c("RNA", "DNA"), limits=c("RNA", "DNA"), labels=c("RNA", "DNA"), values=c(16, 5))
 dev.off()
