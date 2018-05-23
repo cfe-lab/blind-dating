f9d704893b790bcde30cd71659f84cf58115c933
Brad Jones <brj1@sfu.ca>
2018-05-23 12:15:07 -0700

    aded mullins scripts

---
+++ data/mullins/RUN.ogr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --mem=1G
+#SBATCH --cpus-per-task=1
+
+
+raxml=raxmlHPC-PTHREADS-AVX
+pat_id_short=${SLURM_JOB_NAME}
+pat_id=${pat_id_short}-with_ref
+src=~/blind-dating/src
+rep=1989
+cpus=$SLURM_CPUS_PER_TASK
+
+if [ ! -e trees ]; then mkdir trees; fi
+if [ ! -e trees.rooted ]; then mkdir trees.rooted; fi
+if [ ! -e stats ]; then mkdir stats; fi
+
+#echo "Building Tree"
+#R --slave --silent -f ${src}/build.tree.R --args aligned/${pat_id}.fasta trees/${pat_id}.nwk ${raxml} $cpus ${rep}
+#echo "Rooting"
+#Rscript ${src}/root.tree.R trees/${pat_id}.nwk info/${pat_id_short}.csv trees.rooted/${pat_id}.ogr.nwk 0 0
+echo "Regressing"
+Rscript ${src}/regression.R --tree=trees.rooted/${pat_id}.ogr.nwk --info=info/${pat_id_short}.csv --patid=${pat_id}
+#echo "Plotting"
+#Rscript ${src}/plot.R --tree=trees.rooted/${pat_id}.ogr.nwk --patid=${pat_id}
