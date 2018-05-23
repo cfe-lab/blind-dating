3eda88f382956f968c669522d02aafd8620a76ba
brj1 <brj1@sfu.ca>
2018-05-23 14:57:38 -0700

    updated censor simulated to incorporate KEPT sequences only

--- src/censor.simulated.R
+++ src/censor.simulated.R
@@ -10,6 +10,6 @@ set.seed(seed)
 
 info.files <- dir(info.path, full.names=T)
 info <- lapply(info.files, read.csv)
-info <- lapply(info, function(x) {y <- x; y[sample.int(nrow(x), censor), 'CENSORED'] <- 1; y})
+info <- lapply(info, function(x) {y <- x; y[which(x$KEPT == 1)[sample.int(sum(x$KEPT), censor)], 'CENSORED'] <- 1; y})
 
 suppress <- lapply(1:length(info), function(i) write.csv(info[[i]], gsub(".csv", ".cens.csv", info.files[[i]]), row.names=F))
