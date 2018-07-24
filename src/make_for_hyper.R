784605db69d003be46a869122e3108c31e3f69c9
brj1 <brj1@sfu.ca>
2018-07-24 10:23:02 -0700

    removed old make hypermut script and embelished new olne

--- src/make_for_hyper.R
+++ src/make_for_hyper.R
@@ -8,17 +8,29 @@ op <- OptionParser()
 op <- add_option(op, "--fasta", type='character')
 op <- add_option(op, "--info", type='character')
 op <- add_option(op, "--hyperfasta", type='character')
+op <- add_option(op, "--real", type='logical', action='store_true', default=F)
+op <- add_optoon(op, "--useall", type='logical', action='store_true', default=F)
 args <- parse_args(op)
 
 fasta.file <- args$fasta
 info.file <- args$info
 hyper.fasta.file <- args$hyperfasta
+use.date <- args$real
+use.all <- args$use.all
 
 f <- read.fasta(fasta.file)
-info <- read.info(info.file, names(f))
 
-info$COLDATE <- as.Date(info$COLDATE)
+if (!use.all) {
+	info <- read.info(info.file, names(f))
 
-ref <- list(consensus(do.call(rbind, f[info$COLDATE == min(info$COLDATE)])))
+	if (use.date)
+		info$COLDATE <- as.Date(info$COLDATE)
+
+	f.clade <- f[info$COLDATE == min(info$COLDATE)]
+} else {
+	f.clade <- f
+}
+
+ref <- list(consensus(do.call(rbind, f.clade)))
 
 write.fasta(c(ref, f), c("REFERENCE", names(f)), hyper.fasta.file)
