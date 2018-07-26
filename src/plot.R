ad2ee5146b02d906e6eb094c64c54e4f14e22f48
brj1 <brj1@sfu.ca>
2018-07-26 13:15:38 -0700

    typo

--- src/plot.R
+++ src/plot.R
@@ -662,7 +662,7 @@ if (use.dups) {
 	fort.dup$x.shift <- fort.dup$x
 	fort.dup$my.type <- with(fort.dup, paste0(type, censored > 0))
 	
-	data.comb <- subset(rbind(data[, c("tip.label", "type", "censored", "date", "dist", "est.date", "date.diff", "my.type", "my.colour")], fort.dup[, c("tip.label", "type", "censored", "date", "dist", "est.date", "date.diff", "my.type", "my.colour")])
+	data.comb <- subset(rbind(data[, c("tip.label", "type", "censored", "date", "dist", "est.date", "date.diff", "my.type", "my.colour")], fort.dup[, c("tip.label", "type", "censored", "date", "dist", "est.date", "date.diff", "my.type", "my.colour")]))
 	
 	if (use.rainbow) {
 		data.comb$my.colour <- data.comb$date
@@ -672,6 +672,9 @@ if (use.dups) {
 		my.colour.value <- rep('black', length(my.colour.break))
 		my.colour.scale <- (as.numeric(my.colour.break[my.colour.filter]) - MIN_COL_TIME) / (MAX_COL_TIME - MIN_COL_TIME)
 		my.colour.value[my.colour.filter] <- hsv(my.colour.scale * .75, 0.5, 0.5)
+		
+		fort.dup$my.colour <- fort.dup$date
+		fort.dup$my.colour[fort.dup$censored > 0] <- "censored"
 	} else {
 		fort.dup$my.colour <- fort.dup$type
 	}
