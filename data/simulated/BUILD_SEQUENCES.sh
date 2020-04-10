0aa1872433833896bf627fd929ffe5ea5772f746
brad <brj1@sfu.ca>
2020-04-09 17:39:04 -0700

    added example data

--- data/simulated/BUILD_SEQUENCES.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --mem=1G
-
-rep=$SLURM_ARRAY_TASK_ID
-
-Rscript ../../src/gen_guide_trees.R ${rep} ${rep} 1989 ref/HXB2.fasta
--- data/simulated/do.pat.plot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-src=../../src
-pat_id=SIM_1
-
-Rscript ${src}/plot.R --tree=trees.rooted/${pat_id}.rtt.nwk --patid=${pat_id} --mincoltime=0 --maxcoltime=2304.71091998375 --histfreqby=5 --histheight=1.1 --yearend=2374.125
